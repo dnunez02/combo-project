4116ca95aa5aa58fdf2bd79b3700a392b8ddb9ba
dnunez02 <dan@cs.tufts.edu>
2014-12-24 11:49:30 -0500

    Cleared a memory leak and successful parse of simple moves

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #include "def.h"
 
 int read_move(move_t);
+extern int strtodir(char *);
 
 int main(){
   printf("%i\n", F);
@@ -14,26 +16,31 @@ int main(){
 
 //Reads a Capcom notation move
 int read_move(move_t m){
-  char dir;
-  char atk[1024];
+  char *dir;
+  char *atk;
   int pos;
 
   char *line;
   line = NULL;
 
-  size_t n = 1024;
-
   //Read a line and remove the delimeter
+  size_t n;
   n = getline(&(line), &n, stdin);
   line[n-1] = 0;
 
   //Split around the dot
-  sscanf(line, "%c.%s\n", &dir, atk);
+  dir = line;
+  atk = index(line, '.');
+  if(!atk) {
+    perror("index:");
+    exit(1);
+  }
+  atk[0] = 0;
+  atk++;
 
-  //toupperstr(atk, n-1);
+  printf("dir is %i\n", strtodir(dir));
+  printf("atk is %i\n", strtoatk(atk));
 
-  printf("dir is %c\n", dir);
-  printf("atk is %s\n", atk);
-
+  free(line);
   return 0;
 }
